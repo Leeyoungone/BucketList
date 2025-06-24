3f09c86f0c4cbb3c4a1e7aba1b9fac0f92ad7289
Youngone Lee <youngone.lee@accenture.com>
2025-06-23 23:30:43 -0400

    first commit

---
+++ app/(tabs)/list.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+
+export default function ListScreen() {
+    return(
+        <View> 
+            <Text> this is the list screen</Text>
+        </View>
+    );
+}
